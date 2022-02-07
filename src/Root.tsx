d6d4c956803fb55c64d715acea77a91aafd33535
Ben Lenser <ben.lenser@scout24.com>
2022-02-07 16:18:21 +0100

    Move counters in folder

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import { CounterApp1 } from './Counter';
-import { CounterApp2 } from './Counter2';
-import { CounterApp3Dont } from './Counter3Dont';
-import { CounterApp3PleaseDo } from './Counter3PleaseDo';
-import { Counter4 } from './Counter4/Counter4';
-import { Counter5 } from './Counter5/Counter5';
-import { Counter6 } from './Counter6/Counter6';
+import { CounterApp1 } from './counters/Counter';
+import { CounterApp2 } from './counters/Counter2';
+import { CounterApp3Dont } from './counters/Counter3Dont';
+import { CounterApp3PleaseDo } from './counters/Counter3PleaseDo';
+import { Counter4 } from './counters/Counter4/Counter4';
+import { Counter5 } from './counters/Counter5/Counter5';
+import { Counter6 } from './counters/Counter6/Counter6';
 
 const Root: React.FC = () => {
-  // return <CounterApp1 />;
+  return <CounterApp1 />;
   // return <CounterApp2 />;
   // return <CounterApp3Dont />;
   // return <CounterApp3PleaseDo />;
   // return <Counter4 />;
   // return <Counter5 />;
-  return <Counter6 />;
+  // return <Counter6 />;
 };
 
 export default Root;
